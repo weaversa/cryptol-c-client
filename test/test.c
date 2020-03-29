7d18e49b1cb2454f48dbd332c8e92bc1388e9858
Sean Weaver <saweave@evoforge.org>
2020-03-29 18:58:33 +0000

    working test

--- test/test.c
+++ test/test.c
@@ -12,12 +12,24 @@ int main(int argc, char const *argv[]) {
   
   int cryservfd = CryptolServiceConnect(ip_address, port);
   if(cryservfd == -1) return 0;
-  
-  send(cryservfd, "4:test,", strlen("4:test,"), 0);
+
+  char str[1024] = "{\"jsonrpc\":\"2.0\",\"method\":\"load module\",\"params\":{\"state\":[],\"module name\":\"Primitive::Symmetric::Cipher::Block::AES\"},\"id\":0}";
+  char netstr[1024];
+
+  sprintf(netstr, "%ld:%s,", strlen(str), str);
+  send(cryservfd, netstr, strlen(netstr), 0);
   fprintf(stdout, "Message sent\n");
-  
   char buffer[1024] = {0};
   int valread = read(cryservfd, buffer, 1024);
   fprintf(stderr, "%s\n",buffer);
+
+  char straes[1024] = "{\"jsonrpc\":\"2.0\",\"method\":\"evaluate expression\",\"params\":{\"state\":[[\"load module\",{\"module name\":\"Primitive::Symmetric::Cipher::Block::AES\"}]],\"expression\":{\"expression\":\"call\",\"function\":\"msgToState\",\"arguments\":[{\"expression\":\"bits\",\"encoding\":\"hex\",\"data\":\"0123456789abcdef0123456789abcdef\",\"width\":128}]}},\"id\":1}";
+
+  sprintf(netstr, "%ld:%s,", strlen(straes), straes);  
+  send(cryservfd, netstr, strlen(netstr), 0);
+  fprintf(stdout, "Message sent\n");
+  valread = read(cryservfd, buffer, 1024);
+  fprintf(stderr, "%s\n",buffer);
+  
   return 0;
 }
